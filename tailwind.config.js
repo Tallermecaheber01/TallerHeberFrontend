25435f0a3ade51bcfc858c583b55dd4011e30498
Padilla <paca23919@gmail.com>
2024-10-09 07:17:26 -0600

    Cambios commpletos

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Asegúrate de que Tailwind escanee tus archivos HTML y TypeScript.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
